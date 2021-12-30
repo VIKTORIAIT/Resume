00bc9cd3936e28ee0fdf523e376fdc908f40859e
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2021-12-30 17:34:45 +0200

    add

--- build/static/js/5.1104806f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[5],{235:function(t,e,n){"use strict";n.r(e);n(0);var c=n(193),o=n(2);e.default=function(){return Object(o.jsx)("div",{children:Object(o.jsx)("h1",{style:{marginTop:80},children:Object(o.jsx)(c.a,{variant:"h4",gutterBottom:!0,component:"div",children:"WELLCOME TO PHONEBOOK"})})})}}}]);
-//# sourceMappingURL=5.1104806f.chunk.js.map
--- src/redux/appActions.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-import { FILTER } from "./constantsType";
-
-const Filter = createAction(FILTER);
-export default Filter;
